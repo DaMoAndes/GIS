53d14561db92ede76529c7b8bc1cbd476668d037
David Morales <d.moralesa2@uniandes.edu.co>
2024-03-03 00:34:13 +0000

    Construyendo Seedwork

--- src/GIS/api/modulos/gis/dominio/excepciones.py
+++ src/GIS/api/modulos/gis/dominio/excepciones.py
@@ -1,11 +1,11 @@
-""" Excepciones del dominio de vuelos
+""" Excepciones del dominio de gis
 
 En este archivo usted encontrará los Excepciones relacionadas
-al dominio de vuelos
+al dominio de gis
 
 """
 
-from aeroalpes.seedwork.dominio.excepciones import ExcepcionFabrica
+from GIS.seedwork.dominio.excepciones import ExcepcionFabrica
 
 class TipoObjetoNoExisteEnDominioVuelosExcepcion(ExcepcionFabrica):
     def __init__(self, mensaje='No existe una fábrica para el tipo solicitado en el módulo de vuelos'):
